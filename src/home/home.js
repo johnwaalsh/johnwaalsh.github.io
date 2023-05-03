d65d9991f6dd8163348fe0f9f505acf7a4e19d3e
John <johnwalsh7412@gmail.com>
2023-05-03 17:14:00 -0400

    fix mobile styling

--- src/home/home.js
+++ src/home/home.js
@@ -3,11 +3,11 @@ import React from "react";
 function HomeComponent() {
     return (
         <div className="text-start">
-            <div className="d-flex">
-                <img style={{height: 200}} alt="profile" className="rounded-circle" src="images/profile.png"/>
-                <div className="d-flex flex-column m-3 mt-5">
-                    <span className="mb-3"><b>Hi, I'm John. I'm a Software Engineer and a graduate student at Northeastern University studying Computer Science.</b></span>
-                    <span className="mb-3">I recently graduated from Northeastern University with a Bachelor's in Computer Science concentrating in Artificial Intelligence.</span>
+            <div className="d-flex flex-column flex-md-row">
+                <img style={{height: 200, width: 200}} alt="profile" className="align-self-center align-self-md-start rounded-circle" src="images/profile.png"/>
+                <div className="d-flex flex-column ms-0 ms-md-3 me-0 me-md-3 mb-md-3 mt-5">
+                    <span className="mb-3"><b>Hello! I'm John. I'm a Software Engineer and a graduate student at Northeastern University studying Computer Science.</b></span>
+                    <span className="">I recently graduated from Northeastern University with a Bachelor's in Computer Science concentrating in Artificial Intelligence.</span>
                 </div>
             </div>
             <div className="d-flex flex-column mt-3">
